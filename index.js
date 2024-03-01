9a129c64c708ce32c7c48bd80589c703d00aeef6
TanuKaur <tkaur14151gmail.com>
2024-03-01 12:30:01 +0530

    New Commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+// import routes
+// const blog =require("./routes/blog");
+// // mount
+// app.use("/api/v1",blog);
+
+// connect to db
+const connectWithDB = require("./config/database");
+
+connectWithDB();
+
+// start the server
+app.listen(PORT,()=>{
+    console.log(`Running at PORT no ${PORT}`)
+}
+);
+
+// default route
+app.get("/",(req,res)=>{
+    res.send(`<h1>This is my homepage<h1/>`)
+})
